29a58553ad2de91c9727627238fa62c7aaafc66c
Aman Gautam <aman@pointmotioncontrol.com>
2022-01-09 19:18:51 +0530

    adding root file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', require('./api/auth/login'))
+app.get('/', require('./api/static/root'))
 app.post('/auth/login', require('./api/auth/login'))
 app.post('/auth/register', require('./api/auth/register'))
 
